9b28428fd2f3f6c35c918de3834af68d4c1a2772
David Behar <david@behar.io>
2020-02-18 23:15:09 -0600

    Header styled component failed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import { data } from '../../data.json';
 
 const { name, profession, avatar } = data;
+
+const Title = styled.h1`
+  color:red;
+`;
 
 function Header({ children }) {
   return (
     <div className='Header'>
       <figure className='Header-avatar'><img src={avatar} alt='' /></figure>
 
-      <h1 className='Header-title'>{name}</h1>
+      <Title className='Header-title'>{name}</Title>
       <h2 className='Header-profession'>{profession}</h2>
       {children}
     </div>
